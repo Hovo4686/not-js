492ffc89a098b95cd415d1f18ae9980495a2d435
Hovo4686 <131679116+Hovo4686@users.noreply.github.com>
2023-05-17 13:49:26 +0400

    javascript, which we write function should return us id and context should return new id every time it is called

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,19 @@
 const botEl = document.getElementById('bot');
 
+
+function createNoteEl (id, content) {
+    console.log(id, content);
+
+}
+
 function addNote () {
     //console.log('Clicked');
+    const noteObj = {
+        id: Math.floor(Math.random() * 100000),
+        content: '',   
+    };
+    // console.log(noteObj);
+    const noteEl = createNoteEl(noteObj.id, noteObj.content)
 }
 
 botEl.addEventListener('click', addNote)
