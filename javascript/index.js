8a90db078a0e7857f01f39e4a3d91aa027f86018
Hovo4686 <131679116+Hovo4686@users.noreply.github.com>
2023-05-17 18:10:08 +0400

    վերջ

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,10 @@
 const botEl = document.getElementById('bot');
-const butEl = document.getElementById('but')
+const butEl = document.getElementById('but');
+
+getNotes().forEach((note) => {
+    const noteEl = createNoteEl(note.id, note.content)
+    butEl.insertBefore(noteEl, botEl);
+})
 
 
 function createNoteEl (id, content) {
@@ -22,23 +27,41 @@ function createNoteEl (id, content) {
     return element;
 }
 
-function deleteNote () {
-
+function deleteNote ( id, element) {
+    const notes = getNotes().filter((note) => note.id != id)
+    saveNote(notes)
+    butEl.removeChild(element)
 }
 
-function updateNote () {
-
+function updateNote ( id, content) {
+    const notes = getNotes()
+    const target = notes.filter((note)=>note.id == id)[0];
+    target.content = content;
+    saveNote(notes);
 }
 
 function addNote () {
     //console.log('Clicked');
+    const notes = getNotes();
     const noteObj = {
         id: Math.floor(Math.random() * 100000),
         content: '',   
     };
     // console.log(noteObj);
     const noteEl = createNoteEl(noteObj.id, noteObj.content);
-    butEl.insertBefore(noteEl, botEl)
+    butEl.insertBefore(noteEl, botEl);
+
+    notes.push(noteObj)
+    
+    saveNote(notes);
+}
+
+function saveNote (notes) {
+    localStorage.setItem('note-but', JSON.stringify(notes))
+}
+
+function getNotes () {
+   return JSON.parse(localStorage.getItem('note-but') || '[]');
 }
 
 botEl.addEventListener('click', addNote)
