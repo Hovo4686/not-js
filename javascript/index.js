40ebd0c6a9c67c60074223d49ee2f2af9c3dd21c
Hovo4686 <131679116+Hovo4686@users.noreply.github.com>
2023-05-17 16:26:53 +0400

    In javascript, I added a function to add the note edge, which returns us a new note, and you also have a delete function, which is still incomplete.

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,32 @@
 const botEl = document.getElementById('bot');
+const butEl = document.getElementById('but')
 
 
 function createNoteEl (id, content) {
-    console.log(id, content);
+    // console.log(id, content);
+    const element = document.createElement ('textarea')
+    element.classList.add('note')
+    element.placeholder = 'Empty Note'
+    element.value = content
+
+    element.addEventListener('dblclick', () => {
+        const warning = confirm('Do you want to delete this note ?')
+        if (warning) {
+            deleteNote(id, element)
+        }
+    })
+
+    element.addEventListener('input', () => {
+        updateNote(id, element.value)
+    });
+    return element;
+}
+
+function deleteNote () {
+
+}
+
+function updateNote () {
 
 }
 
@@ -13,7 +37,8 @@ function addNote () {
         content: '',   
     };
     // console.log(noteObj);
-    const noteEl = createNoteEl(noteObj.id, noteObj.content)
+    const noteEl = createNoteEl(noteObj.id, noteObj.content);
+    butEl.insertBefore(noteEl, botEl)
 }
 
 botEl.addEventListener('click', addNote)
